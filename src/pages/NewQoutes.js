5e2ccccb84e054ebc9313ebe4437adaf7f7aab24
Lenarqa <37902865+Lenarqa@users.noreply.github.com>
2022-01-08 17:44:00 +0700

    add quote work with firebase now

--- src/pages/NewQoutes.js
+++ src/pages/NewQoutes.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import QuoteForm from '../components/quotes/QuoteForm';
-import { useHistory }  from "react-router-dom"
+import React, { useEffect } from "react";
+import QuoteForm from "../components/quotes/QuoteForm";
+import { useHistory } from "react-router-dom";
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
 // import classes from './NewQuotes.module.css';
 
 const NewQuotes = (props) => {
-    const history = useHistory();
-    const addNewQouteHandler = (qouteData) => {
-        console.log("addNewQouteHandler");
-        console.log(qouteData);
-        
-        history.push("/quotes");
+  const history = useHistory();
+  const { sendRequest, status } = useHttp(addQuote);
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes");
     }
-    
-    return (
-        <div>
-            <h2>New Qoutes</h2>
-            <QuoteForm onAddQuote={addNewQouteHandler}/>
-        </div>
-    );
+  }, [status, history]);
+  const addNewQouteHandler = (qouteData) => {
+    sendRequest(qouteData);
+  };
+
+  return <QuoteForm isLoading={status === "pending"} onAddQuote={addNewQouteHandler} />;
 };
 
 export default NewQuotes;
