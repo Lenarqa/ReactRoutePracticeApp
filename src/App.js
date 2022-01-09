85fcac893df069ce5f2fe16466b9319f5811e423
Lenarqa <37902865+Lenarqa@users.noreply.github.com>
2022-01-09 23:46:33 +0700

    add lazy loading (react.lazy)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
+import React, { Suspense } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Layout from "./components/layout/Layout";
-import MainNavigation from "./components/layout/MainNavigation";
+// import MainNavigation from "./components/layout/MainNavigation";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
 
-import AllQoutes from "./pages/AllQuotes";
-import NewQuotes from "./pages/NewQoutes";
-import NotFound from "./pages/NotFound";
-import QuotesDetail from "./pages/QuotesDetail";
+// import AllQoutes from "./pages/AllQuotes";
+// import NotFound from "./pages/NotFound";
+// import NewQuotes from "./pages/NewQoutes";
+// import QuotesDetail from "./pages/QuotesDetail";
+
+const NewQuotes = React.lazy(() => import("./pages/NewQoutes"));
+const QuotesDetail = React.lazy(() => import("./pages/QuotesDetail"));
+const AllQoutes = React.lazy(() => import("./pages/AllQuotes"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
 
 function App() {
   return (
-      <Layout>
+    <Layout>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
         <Switch>
           <Route path="/" exact>
             <Redirect to="/quotes" />
@@ -27,7 +41,8 @@ function App() {
             <NotFound />
           </Route>
         </Switch>
-      </Layout>
+      </Suspense>
+    </Layout>
   );
 }
 
